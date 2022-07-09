533f1603b23c6ea4881b128e34cecd6b0c1026c1
Ana Milanezi <anamilanezi@gmail.com>
2022-07-09 16:22:39 -0300

    create module and add draw to result

--- 100-days-of-code/d011-d020/projects/d11-blackjack/main.py
+++ 100-days-of-code/d011-d020/projects/d11-blackjack/main.py
@@ -11,103 +11,9 @@
 ## The computer is the dealer.
 
 from art import *
+from requirements import *
 from random import choice
 import os
-
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-cards_dic = {'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, }  
-
-def get_random_card():
-    return choice(list(cards_dic.keys()))
-
-def start_hand():
-    player_hand = []
-    while len(player_hand) < 2:
-        random_card = get_random_card()
-        player_hand.append(random_card)
-    return player_hand
-
-def get_score(current_cards):
-    score = 0
-    for card in current_cards:
-        score += cards_dic[card]
-
-    if 'A' in current_cards and score > 21:
-        score -= 10
-
-    return score
-
-def add_new_card(current_cards):
-    random_card = choice(list(cards_dic.keys()))
-    current_cards.append(random_card)
-    return current_cards
-
-def format_list(any_list):
-    list_to_str = ' ][ '.join(any_list)
-    return list_to_str
-
-def print_lines():
-    print(f"{'-':-^62}")
-
-def decor_text(any_string, with_line=False):
-    if with_line:
-        print_lines()
-        print(f"{any_string:^62}")
-        print_lines()
-    else:
-        print(f"{any_string:^62}")
-
-def get_result(player_score, computer_score):
-
-    result = ""
-    if player_score > 21:
-        decor_text("You went over.")
-        result = lose
-    elif player_score <= 21 and computer_score > 21:
-        decor_text("Opponent went over.")
-        result = win
-    elif player_score > computer_score:
-        result = win
-    else:
-        result = lose 
-    decor_text(result, True)
-    
-    # print(f"{lines:-^50}\n{result:^50}\n{lines:-^50}")
-
-def game_text(player_hand, player_score, computer_first_card):
-    # print(f"\nYour cards: [ {format_list(player_hand)} ], current score: {player_score}")
-    # print(f"Computer's first card: {computer_first_card}\n")
-    show_player_cards = f"Your cards: [ {format_list(player_hand)} ]"
-    show_player_score = f"Current score: {player_score}"
-    show_pc_card = f"Computer's first card: {computer_first_card}"
-    decor_text(show_player_cards, True)
-    decor_text(show_player_score)
-    decor_text(show_pc_card, True)
-
-
-def final_text(player_hand, player_score, computer_hand, computer_score):
-    # print(f"Your final hand is [ {format_list(player_hand)} ] {'final score:':>15} {player_score}")
-    # print(f"Computer's final hand is [ {format_list(computer_hand)} ] {'final score:':>15} {computer_score} ")
-    show_player_final_hand = f"Your final hand is [ {format_list(player_hand)} ]"
-    show_player_final_score = f"Your final score is {player_score}"
-    show_pc_final_hand = f"Computer's final hand is [ {format_list(computer_hand)} ]"
-    show_pc_final_score = f"Computer final score is {computer_score}"
-    decor_text(show_player_final_hand)
-    decor_text(show_player_final_score)
-    decor_text(show_pc_final_hand)
-    decor_text(show_pc_final_score)
-    
-    get_result(player_score, computer_score)
-
-def wants_to(user_input):
-    if (user_input.lower() == 'y'):
-        return True
-    elif (user_input.lower() == 'n'):
-        return False
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 def start_game():
     play = input("Do you want to play a game of BlackJack? Type 'y' or 'n': ")
@@ -124,7 +30,9 @@ def start_game():
         game_text(player_hand, player_score, computer_first_card)
 
         if player_score == 21:
-            print("BLACKJACK! You win.")
+            decor_text("BLACKJACK!")
+            decor_text(win, True)
+            start_game()
 
         while player_score < 21:
             get_another_card = input("Type 'y' to get another card, type 'n' to pass: ")
@@ -136,12 +44,14 @@ def start_game():
                 game_text(player_hand, player_score, computer_first_card)
 
             if not wants_to(get_another_card):
+                print_lines()
                 break
 
         while computer_score < 17: 
             computer_hand = add_new_card(computer_hand) 
             computer_score = get_score(computer_hand)   
 
+        
         final_text(player_hand, player_score, computer_hand, computer_score)
         start_game()
 
