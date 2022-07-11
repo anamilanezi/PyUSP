21261f6365ca905f86305280b4fd441932a9d1c3
Ana Milanezi <anamilanezi@gmail.com>
2022-07-11 12:58:54 -0300

    refactor guess the number game d12

--- 100-days-of-code/d011-d020/projects/d12-number-guessing-game/main.py
+++ 100-days-of-code/d011-d020/projects/d12-number-guessing-game/main.py
@@ -2,10 +2,15 @@ import random
 from art import logo
 import os
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 def clear():
+    '''Clears the console'''
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def play_again():
+    '''Asks the user if wants to play again after finishing a game'''
     user_input = input("Do you want to play again? Type 'y' or 'n': "). lower()
     if (user_input.lower() == 'y'):
         return True
@@ -17,54 +22,61 @@ def choose_difficulty():
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
     if difficulty == 'easy':
-        attempts = 10
+        attempts = EASY_LEVEL_TURNS
     elif difficulty == 'hard':
-        attempts = 5
+        attempts = HARD_LEVEL_TURNS
+    else:
+        print("Sorry, that's not a valid answer. Please try again.")
+        return choose_difficulty()
     return attempts
 
 def print_lines():
-    print(f"{'-':-^55}")
+    '''Prints a line of hifen for decoration'''
+    print(f"{'-':-^50}")
 
-def game_play(guessed, attempts_remaining, random_number):
-    '''Takes a boolean, number of remaining attempts and the random number, asks for user
-    to guess until finds the right answer or there's no more attempts'''
-    while not guessed:
-
-        while attempts_remaining > 0:
-            print_lines()
-            print(f"You have {attempts_remaining} attempts remaining to guess the number.")
-            
-            guess = int(input("Make a guess: "))
-
-            if guess < random_number:
-                print("Too low.\nGuess again.")
-                attempts_remaining -= 1
-            elif guess > random_number:
-                print("Too high.\nGuess again.")
-                attempts_remaining -= 1
-            elif guess == random_number:
-                guessed = True
-                print(f"YOU GOT IT! The answer was {random_number}.")
-                break
-        if attempts_remaining == 0:
-            print(f"You have no attempts left. The answer was {random_number}.")
-            break
+def check_answer(attempts_remaining, guess, random_number):
+    '''Compares the user answer with the right number, gives a hint and update the number of turns'''
+    print_lines()
+    if guess < random_number:
+        print("Too low.\nGuess again.")
+        turns = attempts_remaining - 1
+        return turns
+    elif guess > random_number:
+        print("Too high.\nGuess again.")
+        turns = attempts_remaining - 1
+        return turns
 
 def start_game():
-    
+    '''Starts the guessing number game'''
+    print_lines()
     print(logo)
     print_lines()
     print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+    print_lines()
 
     random_number = random.randint(1,100)
-    # print(f"Pssst here's the answer: {random_number}")
-
-    guessed_the_number = False
     attempts_remaining = choose_difficulty()
+    guess = 0
+    
+    print_lines()    
+    # print(f"Pssst here's the answer: {random_number}")
+    
+    while guess != random_number and attempts_remaining > 0:
+        print(f"You have {attempts_remaining} attempts remaining to guess the number.")
 
-    game_play(guessed_the_number, attempts_remaining, random_number)    
+        guess = int(input("Make a guess: "))
 
-    print_lines()
+        attempts_remaining = check_answer(attempts_remaining, guess, random_number) 
+        print_lines() 
+    
+    if attempts_remaining == 0:
+        print_lines()
+        print(f"You have no attempts left. The answer was {random_number}.")
+        print_lines()
+
+    if guess == random_number:
+        print(f"YOU GOT IT!!! The answer was {random_number}.")
+        print_lines()
 
     if play_again():
         clear()
