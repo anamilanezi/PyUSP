ea77a7657c67a7ecaaba975fb99998dd847f1422
Ana Milanezi <anamilanezi@gmail.com>
2022-07-12 13:44:36 -0300

    add problems 1052 and 1059

---
+++ beecrowd/1052-mes.py
@@ -0,0 +1,6 @@
+num = int(input())
+
+meses = ['January', 'February ', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+print(f"{meses[num - 1]}\n")
+
