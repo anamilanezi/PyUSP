734380605c83e6214cf4e9128efafb437efa9cc4
Ana Milanezi <anamilanezi@gmail.com>
2022-07-21 13:56:23 -0300

    d21 slicing notes

---
+++ 100-days-of-code/d021-d030/annotations/d21-slicing.py
@@ -0,0 +1,16 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+
+# Get all itens from position 2
+print(piano_keys[2:])
+
+# Get all itens til position 5 (stops at 4)
+print(piano_keys[:5])
+
+# Get itens from 2 to 5 (not included), skipping
+print(piano_keys[2:5:2])
+
+# All list skipping 2
+print(piano_keys[::2])
+
+# Reverte the list
+print(piano_keys[::-1])
