af37a255db201ab8c6affa6b2ad1d44c9b13a018
Ana Milanezi <anamilanezi@gmail.com>
2022-07-25 09:10:10 -0300

    mail merge final version

--- 100-days-of-code/d021-d030/projects/d24-mail-merge/main.py
+++ 100-days-of-code/d021-d030/projects/d24-mail-merge/main.py
@@ -1,8 +1,18 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Get starting letter text content
+with open("./Input/Letters/starting_letter.txt") as letter:
+    letter_txt = letter.read()
+    print(letter_txt)
+
+# Geting the names from file as a list
+with open("./Input/Names/invited_names.txt") as names:
+
+    name_list = names.read().splitlines()
+    print(name_list)
+
+# Changing the placeholder on starting letter and saving a file for each name of list
+for name in name_list:
+    letter = letter_txt.replace("[name]", name)
+    file_name = f"./Output/ReadyToSend/letter_for_{name}.txt"
+    with open(file_name, mode="w") as file:
+        file.write(letter)
+
