221c7175dc2293a9058240d0cebd49e2ab5d25f8
Ana Milanezi <anamilanezi@gmail.com>
2022-07-27 13:44:08 -0300

    change program appearance

--- 100-days-of-code/d021-d030/projects/d27-gui-converter/main.py
+++ 100-days-of-code/d021-d030/projects/d27-gui-converter/main.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 
-FONT = ('Consolas', 10, 'bold')
+FONT = ('Consolas', 10)
 
 
 def convert():
-    miles = float(mile_input.get())
-    km = miles * 1.6
+    miles = float(miles_input.get())
+    km = miles * 1.609
     km_result_label.config(text=f"{km:.2f}")
 
 
@@ -19,9 +19,9 @@ text_label.grid(column=0, row=1)
 km_result_label = tk.Label(text="0", font=FONT)
 km_result_label.grid(column=1, row=1)
 
-mile_input = tk.Entry(width=10)
-mile_input.insert(1, string="0")
-mile_input.grid(column=1, row=0)
+miles_input = tk.Entry(width=10)
+miles_input.insert(1, string="0")
+miles_input.grid(column=1, row=0)
 
 miles_label = tk.Label(text="Miles", font=FONT)
 miles_label.grid(column=2, row=0)
