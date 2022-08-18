f63a0ee60b8f1dd6bced1556abe42fc1cda3b919
Ana Milanezi <anamilanezi@gmail.com>
2022-08-18 15:49:37 -0300

    close browser after finishing

--- 100-days-of-code/d041-d050/projects/d48-cookie-clicker/main.py
+++ 100-days-of-code/d041-d050/projects/d48-cookie-clicker/main.py
@@ -24,12 +24,6 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
 score = driver.find_element(By.ID, "money")
 
-# cursor = driver.find_element(By.CSS_SELECTOR, '#buyCursor b')
-# cursor_price = get_price(cursor)
-#
-# grandma = driver.find_element(By.CSS_SELECTOR, '#buyGrandma b')
-# grandma_price = get_price(grandma)
-
 store = driver.find_elements(By.CSS_SELECTOR, "#store div b")
 
 #store_dic = {}
@@ -50,6 +44,7 @@ names.reverse()
 prices.reverse()
 
 timeout = time.time() + 5
+
 five_min = time.time() + 60*5
 print(timeout)
 
@@ -57,13 +52,14 @@ while True:
     cookie.click()
     if time.time() > timeout:
         for i in range(len(names)):
-            if int(score.text) >= prices[i]:
+            if int(score.text.replace(",", "")) >= prices[i]:
                 link = driver.find_element(By.ID, f"buy{names[i]}")
                 link.click()
                 timeout += 5
                 break
-        if time.time() > five_min:
-            cookie_per_s = driver.find_element(By.ID, "cps").text
-            print(cookie_per_s)
-            break
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(By.ID, "cps").text
+        print(cookie_per_s)
+        driver.quit()
+        break
 
